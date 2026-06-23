98c333cac0aedd73dc1456cdadc21b1db47b0450
Burhansykes <burhanjt729@gmail.com>
2026-06-23 17:12:53 +0700

    Delete WebPortofolio ArifBurhan directory

--- WebPortofolio ArifBurhan/script.js
+++
@@ -1,7 +0,0 @@
-console.log("Portfolio Loaded");
-const menuToggle = document.querySelector(".menu-toggle");
-const sidebar = document.querySelector("#sidebar");
-
-menuToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("active");
-});
